56e2be8bfc95a2d4e93f9a4a56e75ccc4d072e05
Alixxx-please <user012120@gmail.com>
2024-02-08 21:47:27 +0100

    First 2 APIs

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const input = document.getElementById('input');
-const link = document.getElementById('link');
+const links = document.getElementById('links');
 
 window.onload = () => {
     input.focus();
@@ -7,20 +7,22 @@ window.onload = () => {
 
 input.addEventListener('input', (e) => {
     if (e.target.value.length > 0) {
-        link.textContent = e.target.value;
+        links.textContent = e.target.value;
     } else {
-        link.textContent = 'Paste your url here:';
+        links.textContent = 'Paste your url here:';
     };
     input.focus();
 });
 
-input.addEventListener('keydown', (e) => {
+input.addEventListener('keydown', async(e) => {
     if (e.key.toLocaleLowerCase() === 'enter') {
         e.preventDefault();
-        const url = new URL('https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL');
-        url.searchParams.append('long', input.value);
-        console.log(url);
-        api(url);
+        //const url = new URL('https://csclub.uwaterloo.ca/~phthakka/1pt-express/addURL');
+        const url = input.value;
+        //url.searchParams.append('long', input.value);
+        //url.searchParams.append('url', input.value);
+
+        api3(url);
     };
 });
 
@@ -33,8 +35,37 @@ async function api(url) {
     })
     .then(res => res.json())
     .then(data => {
-        link.textContent = '1pt.co/' + data.short;
-        navigator.clipboard.writeText(link.textContent);
+        links.textContent = '1pt.co/' + data.short;
+        navigator.clipboard.writeText(links.textContent);
     })
     .catch(error => console.error('Error:', error));
 };
+
+async function api2(url) {
+    const link = `https://is.gd/create.php?format=json&url=${encodeURIComponent(url)}`
+    await fetch(link)
+    .then(res => res.json())
+    .then(data => {
+        links.textContent = data.shorturl;
+        navigator.clipboard.writeText(data.shorturl);
+    })
+    .catch(error => console.error('Error:', error));
+};
+
+async function api3(url) {
+    const proxy = 'https://api.codetabs.com/v1/proxy?quest=';
+    const link = `https://ulvis.net/api/write/post?url=${encodeURIComponent(url)}`;
+
+    await fetch(proxy + link, {
+        method: 'POST',
+        mode: 'no-cors',    
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(error => console.error('Error:', error));
+}
